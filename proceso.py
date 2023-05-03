9c7051e3c2397a455601c3836f739c3362610326
krenbastida <krenbastida@gmail.com>
2023-05-02 18:58:39 -0600

    Added some comments about the functions
    Co-authored-by: Humberto Hernández Olvera <Palatio93@users.noreply.github.com>

--- proceso.py
+++ proceso.py
@@ -36,3 +36,48 @@ estado se encuentra, en caso de que se quiera cambiar de estado se busca el que 
 y se cambia a la primera posicion de la lista y asi sucesivamente para evitar tantas llamadas
 a una funcion.
 '''
+
+'''
+groups = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+elected_processes = {0: 1, 1: 4, 2: 7}
+
+def request_resource(process_id, resource_id):
+    group_id = resource_id % len(groups)
+    elected_process_id = elected_processes[group_id]
+    # send a request message to the elected process
+    message = f"REQUEST {process_id} {resource_id}"
+    send_message(elected_process_id, message)
+
+def release_resource(process_id, resource_id):
+    group_id = resource_id % len(groups)
+    elected_process_id = elected_processes[group_id]
+    # send a release message to the elected process
+    message = f"RELEASE {process_id} {resource_id}"
+    send_message(elected_process_id, message)
+
+# initialize the queue of pending requests for each group
+pending_requests = [[] for _ in range(len(groups))]
+
+def handle_message(message):
+    command, process_id, resource_id = message.split()
+    group_id = resource_id % len(groups)
+    if command == "REQUEST":
+        # add the request to the pending requests queue
+        pending_requests[group_id].append((process_id, resource_id))
+        # check if this process is next in line to access the resource
+        if pending_requests[group_id][0][0] == process_id:
+            # grant access to the resource
+            grant_resource_access(process_id, resource_id)
+    elif command == "RELEASE":
+        # remove the request from the pending requests queue
+        pending_requests[group_id].pop(0)
+        # check if there are any other pending requests
+        if len(pending_requests[group_id]) > 0:
+            # grant access to the next process in line
+            next_process_id, next_resource_id = pending_requests[group_id][0]
+            grant_resource_access(next_process_id, next_resource_id)
+
+def grant_resource_access(process_id, resource_id):
+    # grant access to the resource
+    print(f"Process {process_id} granted access to resource {resource_id}")
+'''
