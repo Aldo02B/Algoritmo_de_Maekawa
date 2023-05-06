a8e7be47edbba8ea82caf53f7709567effc8cbd4
krenbastida <krenbastida@gmail.com>
2023-05-05 23:42:29 -0600

    Sanders más o menos

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+#Examen Parcial
+#Miembros del equipo:
+#Bastida Vargas Karen Mariel
+#Gomez Bolaños Luis Aldo
+#Hernández Olvera Humberto Ignacio
+
 from proceso import proceso
 
 def main():
   #Conjuntos de votación
   print("\nExamen Parcial: Algoritmos de exlusión mutua\n")
-  decision = 0
-  while(decision != 4):
+  while(True):
     decision = int(input("\nElije una opción: \n1. Para 4 procesos \n2. Para 3 procesos \n3. Para mejora de Sanders \n4. Salir\nR = "))
     if decision == 1:
       cuatro_procesos()
@@ -12,9 +17,10 @@ def main():
       tres_procesos()
     elif decision == 3:
       sanders()
+    elif decision == 4:
+      return
     else:
       print("Decisión no válida\n")
-  return
 
 ###################################################################################################################################
 ###################################################################################################################################
@@ -218,6 +224,57 @@ def tres_procesos():
 
 def sanders():
   print("Algoritmo de Maekawa con la mejora de Sanders")
+  count = 1
+  # Se agregan las marcas de tiempo.
+  p1 = proceso("p1")
+  p2 = proceso("p2")
+  p3 = proceso("p3")
+  lista_procesos=[p1, p2, p3]
+  print("Se crean los grupos de votación\n")
+  v1=[p1, p2]
+  print("El grupo 1 está constituido de los procesos: P1 y P2")
+  v2=[p2, p3]
+  print("El grupo 2 está constituido de los procesos: P2 y P3")
+  v3=[p3, p1]
+  print("El grupo 3 está constituido de los procesos: P3 y P1")
+
+  p1.agrega_grupo_votacion(v1)
+  p2.agrega_grupo_votacion(v2)
+  p3.agrega_grupo_votacion(v3)
+
+  # 1 -> Proceso 1 quiere entrar a la seccion critica
+  # 2 -> Proceso 2 quiere entrar a la seccion critica
+  # 3 -> Proceso 3 quiere entrar a la seccion critica
+
+  print("El paso 1 es: Proceso 1 quiere entrar a la sección crítica")
+  print("El paso 2 es: Proceso 2 quiere entrar a la sección crítica")
+  print("El paso 3 es: Proceso 3 quiere entrar a la sección crítica")
+
+  print("================================================== ESTADO INICIAL ======================================================")
+  print("Todos los procesos están en estado Released, sus colas de mensajes están vacías y votación en False")
+  imprimir(p1)
+  imprimir(p2)
+  imprimir(p3)
+
+  print("\n================================================== PASO 1 ==================================================")
+  print("---------------------------------------- P1 QUIERE ENTRAR -------------------------------------")
+  print("P1 manda mensaje a todos en su grupo de votación (P2) de que quiere entrar, cambia su estado a Wanted y su votación a True")
+     # Si ambos procesos piden entrar a la zona crítica después de esas dos funciones mandamos a llamar a otra donde escoge quien
+     # entrará a la SC, aquí se realizara la asignación de marcas de tiempo.
+  envia_mensaje_quiero_entrar(p2)
+  count = asigna_marca_tiempo(p2, count)
+  envia_mensaje_quiero_entrar(p3)
+  count = asigna_marca_tiempo(p3, count)
+  envia_mensaje_quiero_entrar(p1)
+  count = asigna_marca_tiempo(p1, count)
+
+  compara_marca_tiempo(p2, p3)
+  compara_marca_tiempo(p3,p1)
+  
+  imprimir(p1)
+  imprimir(p2)
+  imprimir(p3)
+
   return
 
 ###################################################################################################################################
@@ -258,6 +315,27 @@ def envia_mensaje_sali(proceso):
     process.msg.pop()
     process.cambia_votacion_false()
   return
+
+
+def asigna_marca_tiempo(proceso, count):
+  proceso.marca_tiempo = count
+  count = count + 1
+  return count
+
+#Esta función compara la marca de tiempo de los procesos que se pasan como parámetro, para colocar cuál de ellos es el primero
+#en entrar en la sección crítica, entonces se llamaría a la función envían respuesta, que envía la respuesta con la votación
+#y se deja entrar con la función puede entrar, que cambia su estado a Held
+def compara_marca_tiempo(proceso1, proceso2):
+  if(proceso1.marca_tiempo < proceso2.marca_tiempo):
+    envian_respuesta(proceso1)
+    proceso1.puede_entrar()
+  else:
+    envian_respuesta(proceso2)
+    proceso2.puede_entrar()
+
+
+def imprimir(proceso):
+  print("| Nombre = ", proceso.nombre , "| Estado " , proceso.estado , "| Cola de mensajes ", proceso.msg , "| Votacion " , proceso.votacion , " | Marca de Tiempo " , proceso.marca_tiempo, "\n")
 
 if __name__ == '__main__':
   main()
--- proceso.py
+++ proceso.py
@@ -1,3 +1,9 @@
+# Primer Examen Parcial
+# Miembros del equipo:
+#   Bastida Vargas Karen Mariel
+#   Gomez Bolaños Luis Aldo
+#   Hernández Olvera Humberto Ignacio
+
 class proceso:
   def __init__(self, nombre):
     self._nombre = nombre
@@ -5,6 +11,7 @@ class proceso:
     self.msg = []
     self.votacion = "False"
     self.grupo_votacion = []
+    self.marca_tiempo = 0
 
   # Getter para el nombre del proceso
   @property
@@ -64,57 +71,3 @@ class proceso:
 
   def __str__(self):
     return f'| Nombre = {self._nombre} | Estado = {self.estado} | Cola de mensajes = {self.msg} | Votacion = {self.votacion} |'
-
-'''
-Otra implementacion es que para no llamar a tantas funciones para cambiar el estado del proceso
-podemos definir como atributo una lista de los estados: RELEASED, WANTED Y HELL, para saber 
-en que estado se encuentra el proceso podemos ver el primer elemento de la lista donde nos dice que
-estado se encuentra, en caso de que se quiera cambiar de estado se busca el que se quiere cambiar en la lista
-y se cambia a la primera posicion de la lista y asi sucesivamente para evitar tantas llamadas
-a una funcion.
-'''
-
-'''
-groups = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-elected_processes = {0: 1, 1: 4, 2: 7}
-
-def request_resource(process_id, resource_id):
-    group_id = resource_id % len(groups)
-    elected_process_id = elected_processes[group_id]
-    # send a request message to the elected process
-    message = f"REQUEST {process_id} {resource_id}"
-    send_message(elected_process_id, message)
-
-def release_resource(process_id, resource_id):
-    group_id = resource_id % len(groups)
-    elected_process_id = elected_processes[group_id]
-    # send a release message to the elected process
-    message = f"RELEASE {process_id} {resource_id}"
-    send_message(elected_process_id, message)
-
-# initialize the queue of pending requests for each group
-pending_requests = [[] for _ in range(len(groups))]
-
-def handle_message(message):
-    command, process_id, resource_id = message.split()
-    group_id = resource_id % len(groups)
-    if command == "REQUEST":
-        # add the request to the pending requests queue
-        pending_requests[group_id].append((process_id, resource_id))
-        # check if this process is next in line to access the resource
-        if pending_requests[group_id][0][0] == process_id:
-            # grant access to the resource
-            grant_resource_access(process_id, resource_id)
-    elif command == "RELEASE":
-        # remove the request from the pending requests queue
-        pending_requests[group_id].pop(0)
-        # check if there are any other pending requests
-        if len(pending_requests[group_id]) > 0:
-            # grant access to the next process in line
-            next_process_id, next_resource_id = pending_requests[group_id][0]
-            grant_resource_access(next_process_id, next_resource_id)
-
-def grant_resource_access(process_id, resource_id):
-    # grant access to the resource
-    print(f"Process {process_id} granted access to resource {resource_id}")
-'''
